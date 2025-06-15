8a74b04c98401f7a5c74279e288f418f47fd9be3
23f1000793 <23f1000793@ds.study.iitm.ac.in>
2025-06-15 23:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -754,4 +754,4 @@ async def serve_home():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
